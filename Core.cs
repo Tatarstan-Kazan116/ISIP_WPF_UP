de04e37ba29b70cc7b43fe399e91c8864bd352e2
almazsiraziev1@gmail.com <almazsiraziev1@gmail.com>
2026-05-15 12:06:28 +0300

    бд, ссылки

---
+++ Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_New
+{
+    internal class Core
+    {
+        public static UP_RW_SirazievEntities Context = new UP_RW_SirazievEntities();
+    }
+}
